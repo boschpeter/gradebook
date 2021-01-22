d44cf9cd0e2025ce828764c2997d9c06439ea8e5
boschpeter <bosch.peter@icloud.com>
2021-01-22 20:03:19 +0100

    double

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,11 +8,33 @@ namespace GradeBook
         {
             if (args.Length > 0)
             {
-            Console.WriteLine($"Hello {args[0]} nice to see you again");
+
+            //  double x = 6.7 ;
+            //  double y = 7.7 ;
+            //  var  z = x + y;
+            //  Console.WriteLine(z);
+      
+
+                var numbers  = new double[3] {12.7, 10.3, 6.11} ;
+                //numbers[0] = 12.7;
+                //numbers[1] = 10.3;
+                //numbers[2] = 6.11;
+               //
+                var result =  numbers[0];
+                result = result + numbers[1];
+                result = result + numbers[2]; 
+
+                Console.WriteLine(result);
+
+                Console.WriteLine($"Hello {args[0]} nice to see you again");
+             
+
             }
-            else {
-                Console.WriteLine ("helleu");
+            else
+            {
+                Console.WriteLine("helleu");
             }
         }
     }
 }
+
