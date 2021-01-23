311c233c9aac9e2e6583be98e63af0d2642ebb36
ezahr <bosch.peter@icloud.com>
2021-01-23 18:28:33 +0100

    book.cs

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 //git clone git@github.com:boschpeter/gradebook.git
+
 namespace GradeBook
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
-            if (args.Length > 0)
-            {
+           
+         var book = new Book ();
+         book.AddGrade(89.1);
 
-            //  double x = 6.7 ;
-            //  double y = 7.7 ;
-            //  var
-               // var numbers  = new [] {12.7, 10.3, 6.1} ;
-               // List <double> grades = new List<double> ();  // control .
-                var grades = new List<double> () {12.7, 10.3, 6.1} ;
-               // grades.Add(56.1)
+          var grades = new List<double> () {12.7, 10.3, 6.11, 4.1} ;
+          grades.Add(56.1);
 
                 var result =  0.0; 
                 foreach (var number in grades)
@@ -29,17 +26,7 @@ namespace GradeBook
              //  result = result / grades.Count;‹
               result /= grades.Count;
 
-
              Console.WriteLine($"The average grade is {result:N3}");
-
-               Console.WriteLine($"Hello {args[0]} nice to see you again");
-             
-
-            }
-            else
-            {
-                Console.WriteLine("helleu");
-            }
         }
     }
 }
