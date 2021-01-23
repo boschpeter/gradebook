305308697c4811e5973853de1b85aad367938020
ezahr <bosch.peter@icloud.com>
2021-01-23 19:05:00 +0100

    highgrade
    \

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -13,19 +13,24 @@ namespace GradeBook
          book.AddGrade(89.1);
 
           var grades = new List<double> () {12.7, 10.3, 6.11, 4.1} ;
-          grades.Add(56.1);
+          grades.Add(56.1); 
+          var result =  0.0;
+          var highGrade = double.MinValue;
+          var lowGrade = double.MaxValue;
 
-                var result =  0.0; 
                 foreach (var number in grades)
             {
 
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
                 result += number;
 
             }
                
              //  result = result / grades.Count;‹
               result /= grades.Count;
-
+            Console.WriteLine($"the lowest rate is {lowGrade}");
+            Console.WriteLine($"the highest rate is {highGrade}");
              Console.WriteLine($"The average grade is {result:N3}");
         }
     }
