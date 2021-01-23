31465764514aa15e2947b6b49433e18aeaed1d9b
ezahr <bosch.peter@icloud.com>
2021-01-23 15:47:31 +0100

    Merge branch 'main' of github.com:boschpeter/gradebook into main

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -11,18 +11,13 @@ namespace GradeBook
 
             //  double x = 6.7 ;
             //  double y = 7.7 ;
-            //  var  z = x + y;
-            //  Console.WriteLine(z);
-      
-
-                var numbers  = new double[3] {12.7, 10.3, 6.1} ;
-                //numbers[0] = 12.7;
-                //numbers[1] = 10.3;
-                //numbers[2] = 6.11;
+            //  var
+                var numbers  = new double[] {12.7, 10.3, 6.1} ;
                //
                 var result =  numbers[0];
-                result = result + numbers[1];
-                result = result + numbers[2]; 
+                result += numbers[1];
+                result += numbers[2];
+                result += numbers[3];
 
                 Console.WriteLine(result);
 
