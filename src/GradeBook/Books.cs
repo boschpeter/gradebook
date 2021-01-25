1e7cd16dafe3be4bbff3deda7377bdfea884cef6
boschpeter <bosch.peter@icloud.com>
2021-01-25 15:21:29 +0100

    alias dnt='clear && dotnet build && dotnet test'

--- src/GradeBook/Books.cs
+++ src/GradeBook/Books.cs
@@ -3,55 +3,56 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
- public class Book {
+    public class Book
+    {
 
-     public Book (string name )
-     {
-      grades = new List<double> ();
-      this.name = name;
-     }
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
 
-     public void AddGrade (double grade)
+        public void AddGrade(double grade)
 
-     { 
-        grades.Add(grade);
-     }
+        {
+            grades.Add(grade);
+        }
 
-   //public class Statistics
-   // {
-   //     public double Average;
-   //     public double High;
-   //     public double Low;
-//
-   // }
+        //public class Statistics
+        // {
+        //     public double Average;
+        //     public double High;
+        //     public double Low;
+        //
+        // }
 
-     public Statistics GetStatistics()
-     {
+        public Statistics GetStatistics()
+        {
 
             var result = new Statistics();
-            result.Average =  0.0;
+            result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-      foreach (var grade in grades)
-        {
+            foreach (var grade in grades)
+            {
 
-             result.Low = Math.Min(grade, result.Low);
-             result.High= Math.Max(grade, result.High);
-             result.Average += grade;
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
 
-           }
-             
-           
-              result.Average /= grades.Count;
+            }
+
+
+            result.Average /= grades.Count;
             return result;
             //           Console.WriteLine($"the lowest rate is {lowGrade}");
             //           Console.WriteLine($"the highest rate is {highGrade}");
             //           Console.WriteLine($"The average grade is {result:N3}");
         }
-    
-    static private List<double> grades;
-    private string name;
 
- }
+        static private List<double> grades;
+        public string Name;
+
+    }
 }
