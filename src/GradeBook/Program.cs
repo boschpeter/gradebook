769f20f4f593fe8cae017832fdaa294c88e877fb
ezahr <bosch.peter@icloud.com>
2021-01-26 15:14:42 +0100

    OO APIE

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,81 +9,84 @@ namespace GradeBook
     {   
         static void Main(string[] args)
         {
-           
-         var book = new Book ("Scotts grade book");
-         
-         
-         // book.AddGrade(89.1);
-         // book.AddGrade(90.5);
-         // book.AddGrade(77.4);
-         // book.AddGrade(77.6);
-    
-//var done = false;
-//           while (!done)
-//           {
-//            Console.WriteLine("Enter a grade or 'q' to quit ");   
-//            var input = Console.ReadLine();
-//            if (input == "q")
-//            {
-//               done = true;
-//               continue;
-//
-//            }
-//            var grade = double.Parse(input);
-//           }
-//           
 
-            while (true)
-           {
-            Console.WriteLine("Enter a grade or 'q' to quit ");   
-            var input = Console.ReadLine();
-            if (input == "q")
-            {
-              break;
-            }
-            
-            try
-            {
-                var grade = double.Parse(input);
-                book.AddGrade(grade);
-            }
-
-            catch ( ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-                //throw;
-            }
-
-            catch ( FormatException  ex)
-            {
-                Console.WriteLine(ex.Message);
-                //throw;
-            }
-            finally
-            {
-                    System.Console.WriteLine("**");
-
-            }
-
-//            catch ( Exception ex)
-//            {
-//                Console.WriteLine(ex.Message);
-//                throw;
-//            }
-            
-            
-           }
+            var book = new Book("Scotts grade book");
 
 
+            // book.AddGrade(89.1);
+            // book.AddGrade(90.5);
+            // book.AddGrade(77.4);
+            // book.AddGrade(77.6);
+
+            //var done = false;
+            //           while (!done)
+            //           {
+            //            Console.WriteLine("Enter a grade or 'q' to quit ");   
+            //            var input = Console.ReadLine();
+            //            if (input == "q")
+            //            {
+            //               done = true;
+            //               continue;
+            //
+            //            }
+            //            var grade = double.Parse(input);
+            //           }
+            //           
+
+            EnterGrades(book);
 
             var stats = book.GetStatistics();
             book.Name = "tt";
             Console.WriteLine($"the name of the book is {book.Name}");
-             Console.WriteLine($"the lowest rate is {stats.Low}");
-             Console.WriteLine($"the highest rate is {stats.High}");
-             Console.WriteLine($"The average grade is {stats.Average:N1}");
-             Console.WriteLine($"the letter grade is {stats.letter} "); 
+            Console.WriteLine($"the lowest rate is {stats.Low}");
+            Console.WriteLine($"the highest rate is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"the letter grade is {stats.letter} ");
 
+        }
+
+        private static void EnterGrades(Book book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit ");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //throw;
+                }
+
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //throw;
+                }
+                finally
+                {
+                    System.Console.WriteLine("**");
+
+                }
+
+                //            catch ( Exception ex)
+                //            {
+                //                Console.WriteLine(ex.Message);
+                //                throw;
+                //            }
+
+
+            }
         }
     }
 }
