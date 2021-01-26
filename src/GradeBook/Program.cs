dea6304fea55493ad3e1ca1ea67e02129cbeddd9
ezahr <bosch.peter@icloud.com>
2021-01-26 10:52:43 +0100

    dotnet run --project src/GradeBook/GradeBook.csproj

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,10 +10,41 @@ namespace GradeBook
         {
            
          var book = new Book ("Scotts grade book");
-         book.AddGrade(89.1);
-         book.AddGrade(90.5);
-         book.AddGrade(77.4);
-         book.AddGrade(77.6);
+         
+         
+         // book.AddGrade(89.1);
+         // book.AddGrade(90.5);
+         // book.AddGrade(77.4);
+         // book.AddGrade(77.6);
+    
+//var done = false;
+//           while (!done)
+//           {
+//            Console.WriteLine("Enter a grade or 'q' to quit ");   
+//            var input = Console.ReadLine();
+//            if (input == "q")
+//            {
+//               done = true;
+//               continue;
+//
+//            }
+//            var grade = double.Parse(input);
+//           }
+//           
+
+            while (true)
+           {
+            Console.WriteLine("Enter a grade or 'q' to quit ");   
+            var input = Console.ReadLine();
+            if (input == "q")
+            {
+              break;
+            }
+            var grade = double.Parse(input);
+            book.AddGrade(grade);
+           }               
+           
+
 
             var stats = book.GetStatistics();
          
